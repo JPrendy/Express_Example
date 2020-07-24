2fa11fc3d58204de7185c10c555277998a18f22f
James Prendergast <jamprendergast@gmail.com>
2020-07-24 17:05:16 +0100

    test

--- app.js
+++ app.js
@@ -1,9 +1,47 @@
 //need to install ejs   'npm install ejs'
 //npm to install body-parser
 
+// 545  node app.js
+// 546  npm install --save xml-js
+// 547  npm install node-fetch --save
+// 548  node app.js
+// 549  npm install express --save
+// 550  node app.js
+// 551  npm install ejs --save
+// 552  node app.js
+
+
 let express = require("express");
 let app = express(); //executes all the express files whenever the variable app is called
 let bodyParser = require("body-parser");
+
+const fetch = require('node-fetch');
+const convert = require('xml-js')
+function sendRss(url, channelName){
+fetch(url).then(response => response.text()).then(str => {
+    dataAsJson = JSON.parse(convert.xml2json(str));
+    console.log(dataAsJson.elements[0].elements[3].elements[0].text)
+    // console.log(dataAsJson.elements[0].elements[4])
+    // console.log(dataAsJson.elements[0].elements[5].elements)
+    console.log(dataAsJson.elements[0].elements[7].elements[3].elements[0].text) //Channel's name
+    console.log(dataAsJson.elements[0].elements[7].elements[4].attributes.href) //Last Video
+    console.log(dataAsJson.elements[0].elements[7].elements[6].elements[0].text) //Upload date
+
+   data.push(dataAsJson.elements[0].elements[7].elements[3].elements[0].text);
+   console.log("----------")
+   console.log(data[0]);
+})
+}
+
+
+var data = [];
+
+
+sendRss('https://www.youtube.com/feeds/videos.xml?channel_id=UCqJ-Xo29CKyLTjn6z2XwYAw', 'Game Makers Toolkit')
+sendRss('https://www.youtube.com/feeds/videos.xml?channel_id=UCddiUEpeqJcYeBxX1IVBKvQ', 'The Verge');
+sendRss('https://www.youtube.com/feeds/videos.xml?channel_id=UCrqAGUPPMOdo0jfQ6grikZw', 'Colt Steele');
+sendRss('https://www.youtube.com/feeds/videos.xml?channel_id=UC0fDG3byEcMtbOqPMymDNbw', 'No Clip');
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -16,8 +54,9 @@ let posts2 = [ "Harry Potter", "Star Wars"];
 //define a route
 app.get("/", function(req, res){
     //res.send("Hi There!");
-    let name = "James";
-    res.render("home.ejs", {nameVar : name});
+    let name = dataAsJson.elements[0].elements[7].elements[6].elements[0].text;
+    let name2 = dataAsJson.elements[0].elements[7].elements[4].attributes.href;
+    res.render("home.ejs", {posts2 : data});
 });
 
 app.get("/post", function(req, res){
